7fd4ab7af395053f636d4397fe25706101eedf72
Alice Mastrilli <alice.mastrilli@studio.unibo.it>
2023-02-27 11:27:53 +0100

    created test class and tested starter configuration

--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -1,17 +1,32 @@
+import lab01.tdd.CircularList;
+import lab01.tdd.CircularListImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
 
-    //TODO: test implementation
-
+    private CircularList circularList;
     @Disabled
     @Test public void testTodo(){
         Assertions.fail();
     }
 
+    @BeforeEach
+    void beforeEach(){
+        circularList = new CircularListImpl();
+    }
+
+    @Test
+    void testListInitiallyEmpty(){
+        assertEquals(0, this.circularList.size());
+    }
 }
